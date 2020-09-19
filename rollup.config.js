5e32019488e17123df01f3be919e3225602f315c
jueinin <1014397160@qq.com>
2020-09-19 07:49:49 +0000

    chore: import rollup to build code

---
+++ rollup.config.js
@@ -0,0 +1,6 @@
+export default {
+    input: 'src/query/index.ts',
+    output: {
+        dir: "./build"
+    }
+}
---
+++ src/query/index.ts
@@ -0,0 +1,5 @@
+import { useQuery } from "./vue/useQuery";
+import { useMutation } from './vue/useMutation';
+import { queryCache } from './core/queryCache';
+import { useIsFetching } from './vue/useIsFetching';
+export {useQuery,useIsFetching,useMutation,queryCache}
